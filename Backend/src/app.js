ac5593c358896dc83aabc52fe6a3a90930ea5fd0
David_one8 <davidfule18@gmail.com>
2025-09-04 19:35:10 +0530

    Add app.js - Express application configuration and middleware setup

---
+++ Backend/src/app.js
@@ -0,0 +1,18 @@
+// In this file we will create the Express server 
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes')
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
+
+// Dummy route
+app.get('/', (req, res) => {
+  res.send('Hello from the API!');
+});
+
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
