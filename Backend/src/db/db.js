6c224304563a4cbc94629ae4855b3ab08694ecd8
David_one8 <davidfule18@gmail.com>
2025-09-04 19:35:19 +0530

    Add database connection configuration using MongoDB and Mongoose

---
+++ Backend/src/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose'); 
+
+
+function connectDB(){
+  mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+      console.log('MongoDB connected successfully');
+    })
+    .catch((error) => {
+      console.error('MongoDB connection error:', error);
+    });
+}
+
+module.exports = connectDB;
