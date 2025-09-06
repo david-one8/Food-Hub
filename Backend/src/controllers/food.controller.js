c14c9ace04bbc3ad53d84868a9cbf2218d1f0c91
David_one8 <davidfule18@gmail.com>
2025-09-06 15:59:17 +0530

    feat(controller): add food controller

--- Backend/src/controllers/food.controller.js
+++ Backend/src/controllers/food.controller.js
@@ -1,7 +1,14 @@
 const foodModel = require('../models/fooditem.model');
 
 
-async function createFoodItem(req, res){}
+async function createFoodItem(req, res){
+
+  console.log(req.foodPartner);
+
+  console.log(req.body);
+
+  res.send('Food Item Created Successfully');
+}
 
 
 module.exports = {
