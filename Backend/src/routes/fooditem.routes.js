975c0e8b1cf087ee3868dbd1be84892bdee5d924
David_one8 <davidfule18@gmail.com>
2025-09-07 15:07:15 +0530

    refactor(fooditem): clean up fooditem routes for improved readability

--- Backend/src/routes/fooditem.routes.js
+++ Backend/src/routes/fooditem.routes.js
@@ -1,18 +1,25 @@
-const express = require('express');
-const foodController = require('../controllers/food.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
+const express = require("express");
+const foodController = require("../controllers/food.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
 
 const upload = multer({
   storage: multer.memoryStorage(),
-})
+});
+
+// POST /api/fooditem/   [This API should be protected]  
+router.post("/",
+  authMiddleware.authFoodPartnerMiddleware,
+  upload.single("video"),
+  foodController.createFoodItem
+);
 
 
-// POST /api/fooditem/   [This API should be protected]  //
-router.post('/', authMiddleware.authFoodPartnerMiddleware, upload.single('video'), foodController.createFoodItem);
-
-
-
+// GET /api/fooditem/   [This API should be protected]  
+router.get("/",
+  authMiddleware.authUserMiddleware,
+  foodController.getFoodItems
+)
 
 module.exports = router;
