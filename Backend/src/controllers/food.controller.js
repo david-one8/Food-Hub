ee41bbbf97a658fdd11ccd4147e3fda1b8bb0c2a
David_one8 <davidfule18@gmail.com>
2025-09-07 14:27:58 +0530

    feat(fooditem): enhance createFoodItem function to return detailed response and remove debug logs

--- Backend/src/controllers/food.controller.js
+++ Backend/src/controllers/food.controller.js
@@ -1,25 +1,27 @@
-const foodModel = require('../models/fooditem.model');
-const storageService = require('../services/storage.service');
-const { v4: uuid } = require('uuid');
+const foodModel = require("../models/fooditem.model");
+const storageService = require("../services/storage.service");
+const { v4: uuid } = require("uuid");
 
+async function createFoodItem(req, res) {
+  const fileUploadResult = await storageService.uploadFile(
+    req.file.buffer,
+    uuid()
+  );
 
-async function createFoodItem(req, res){
+  const foodItem = await foodModel.create({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    video: fileUploadResult.url,
+    foodPartner: req.foodPartner._id,
+  });
 
-  console.log(req.foodPartner);
-
-  console.log(req.body);
-  console.log(req.file);
-
-
-  const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid());
-
-
-  console.log(fileUploadResult);
-
-  res.send('Food Item Created Successfully');
+  res.status(201).json({
+    message: "Food Item created successfully",
+    foodItem: foodItem,
+  });
 }
 
-
 module.exports = {
-  createFoodItem
+  createFoodItem,
 };
