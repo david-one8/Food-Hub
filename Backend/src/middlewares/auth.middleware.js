875392e869463bf1ef5dc63552bd1cc4d4919ced
David_one8 <davidfule18@gmail.com>
2025-09-07 15:08:25 +0530

    feat(auth): add authUserMiddleware for user authentication

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const foodPartnerModel = require('../models/foodpartner.model');
+const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
 
@@ -29,6 +30,35 @@ async function authFoodPartnerMiddleware(req, res, next){
 
 }
 
+
+async function authUserMiddleware(req, res, next){
+
+  const token = req.cookies.token;
+
+  if(!token){
+    return res.status(401).json({
+      message: 'Please login first'
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await userModel.findById(decoded.id);
+
+    req.user = user;
+
+    next();
+  }
+  catch(err){
+    return res.status(401).json({
+      message: 'Invalid Token'
+    });
+  }
+
+}
+
 module.exports = {
   authFoodPartnerMiddleware,
+  authUserMiddleware
 };
