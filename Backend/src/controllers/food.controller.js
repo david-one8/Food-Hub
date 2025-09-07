8b186a1ab5da741693d8767cca99cf95acbbd37d
David_one8 <davidfule18@gmail.com>
2025-09-07 13:23:20 +0530

    feat(fooditem): add video upload functionality to createFoodItem route

--- Backend/src/controllers/food.controller.js
+++ Backend/src/controllers/food.controller.js
@@ -6,6 +6,7 @@ async function createFoodItem(req, res){
   console.log(req.foodPartner);
 
   console.log(req.body);
+  console.log(req.file);
 
   res.send('Food Item Created Successfully');
 }
--- Backend/src/routes/fooditem.routes.js
+++ Backend/src/routes/fooditem.routes.js
@@ -2,9 +2,15 @@ const express = require('express');
 const foodController = require('../controllers/food.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
+const multer = require('multer');
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+})
+
 
 // POST /api/fooditem/   [This API should be protected]  //
-router.post('/', authMiddleware.authFoodPartnerMiddleware, foodController.createFoodItem);
+router.post('/', authMiddleware.authFoodPartnerMiddleware, upload.single('video'), foodController.createFoodItem);
 
 
 
